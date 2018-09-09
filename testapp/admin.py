323bebaeae455bda9ffb3ae6c41bc01868200b77
Roman Miroshnychenko <roman1972@gmail.com>
2018-09-09 14:04:26 +0300

    Tweak admin interface in the testapp

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
+from modeltranslation.admin import TabbedTranslationAdmin
 from modeltranslation_xliff import XliffExchangeMixin
 from .models import Article
 
 
 @admin.register(Article)
-class ArticleAdmin(XliffExchangeMixin, TranslationAdmin):
-    pass
+class ArticleAdmin(XliffExchangeMixin, TabbedTranslationAdmin):
+    list_display = ('title',)
